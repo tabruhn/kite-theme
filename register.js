c8b3d712c43a411ed33a1b7fe7fd85d818afb6be
aaronjudd <aaronjudd@me.com>
2015-03-11 01:59:37 -0700

    theme updates for guest-checkout branch

--- register.js
+++ register.js
@@ -1,15 +1,8 @@
 // NOT CURRENTLY USED, AS IT WOULD REQUIRE A DEPENDENCY ON CORE.
 ReactionCore.registerPackage({
   name: 'reaction-core-theme',
-  provides: ['theme'],
-  label: 'Core Theme',
-  description: 'Provides a basic customizable theme',
-  icon: 'fa fa-paint-brush',
-  settingsRoute: '',
-  defaultSettings: {
-    mode: true,
-  },
-  cycle: 3,
-  hasWidget: false,
   autoEnable: true
+  settings: {
+    mode: true,
+  }
 });
