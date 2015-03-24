9f115b8c3efd3c92bf6209ec0095eedb26a5a6a6
aaronjudd <aaronjudd@me.com>
2015-03-24 03:26:47 -0700

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "reactioncommerce:core-theme",
   summary: "Reaction Commerce base theme",
-  version: "1.3.1",
+  version: "1.3.2",
   git: "https://github.com/reactioncommerce/core-theme.git"
 });
 
