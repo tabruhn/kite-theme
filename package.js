6ad5284c8de558fa3c77188aa359aa0f116bf318
aaronjudd <aaronjudd@me.com>
2015-04-12 16:03:40 -0700

    minor fix for checkout progress bar z-index

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "reactioncommerce:core-theme",
   summary: "Reaction Commerce base theme",
-  version: "1.3.2",
+  version: "1.3.3",
   git: "https://github.com/reactioncommerce/core-theme.git"
 });
 
